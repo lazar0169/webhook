c73bd165cef9f73af9640946de21d77023f1c6f7
lazar0169 <lazar0169@gmail.com>
2020-06-04 09:48:59 +0200

    Init commit
    (cherry picked from commit ee8b1217bdf224a852d2298f1ff563a984ecbe2c)

---
+++ index.js
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import http from 'http';
+import crypto from 'crypto';
+import {exec} from 'child_process';
+
+const SECRET = process.env.SECRET;
+const PORT = process.env.PORT;
+const SCRIPT_LOCATION = '~';
+
+http.createServer((req, res) => {
+    req.on('data', chunk => {
+        const signature = `sha1=${crypto.createHmac('sha1', SECRET).update(chunk).digest('hex')}`;
+        const isAllowed = req.headers['x-hub-signature'] === signature;
+        const body = JSON.parse(chunk);
+        const isMaster = body?.ref === 'refs/heads/master';
+
+        if (isAllowed && isMaster) {
+            try {
+                exec(`cd ${SCRIPT_LOCATION} && bash deploy.sh`);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    });
+
+    res.end();
+}).listen(PORT);
+
+console.log(`Listening for webhooks on: ${PORT}`);
