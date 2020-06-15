b74c6f29b092df2007b22c7eeca131c278a1cc2e
lazar0169 <lazar0169@gmail.com>
2020-06-15 16:33:20 +0200

    Try/catch block added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import http from 'http';
 import crypto from 'crypto';
 import {exec} from 'child_process';
+
 const util = require('util');
 
 const run = util.promisify(exec);
@@ -15,23 +16,27 @@ let isBusy = false;
 
 http.createServer((req, res) => {
     req.on('data', async chunk => {
-        const signature = `sha1=${crypto.createHmac('sha1', SECRET).update(chunk).digest('hex')}`;
-        const isAllowed = req.headers['x-hub-signature'] === signature;
-        const body = JSON.parse(chunk);
-        const isApprovedBranch = body?.ref === `refs/heads/${BRANCH}`;
+        try {
+            const signature = `sha1=${crypto.createHmac('sha1', SECRET).update(chunk).digest('hex')}`;
+            const isAllowed = req.headers['x-hub-signature'] === signature;
+            const body = JSON.parse(chunk);
+            const isApprovedBranch = body?.ref === `refs/heads/${BRANCH}`;
 
-        console.log(`isAllowed: ${isAllowed}`, `isApprovedBranch: ${isApprovedBranch} (exp: ${body?.ref} req: ${BRANCH})`);
+            console.log(`isAllowed: ${isAllowed}`, `isApprovedBranch: ${isApprovedBranch} (exp: ${body?.ref} req: ${BRANCH})`);
 
-        if (isAllowed && isApprovedBranch) {
-            try {
-                if (!isBusy) {
-                    console.log('Auto deploy started');
-                    isBusy = true;
-                    await run(`cd ${SCRIPT_LOCATION} && bash deploy.sh`);
+            if (isAllowed && isApprovedBranch) {
+                try {
+                    if (!isBusy) {
+                        console.log('Auto deploy started');
+                        isBusy = true;
+                        await run(`cd ${SCRIPT_LOCATION} && bash deploy.sh`);
+                    }
+                } catch (error) {
+                    console.error(error);
                 }
-            } catch (error) {
-                console.error(error);
             }
+        } catch (e) {
+            console.log(JSON.stringify(e));
         }
     });
 
