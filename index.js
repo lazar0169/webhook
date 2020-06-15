459264c8640d8233a16c9dd84ecd4b330188d479
lazar0169 <lazar0169@gmail.com>
2020-06-15 16:46:00 +0200

    Update of try/catch

--- index.js
+++ index.js
@@ -17,33 +17,35 @@ let timestamp = 0;
 
 http.createServer((req, res) => {
     req.on('data', async chunk => {
-        timestamp = Date.now();
+        let body;
+        const signature = `sha1=${crypto.createHmac('sha1', SECRET).update(chunk).digest('hex')}`;
+        const isAllowed = req.headers['x-hub-signature'] === signature;
         try {
-            const signature = `sha1=${crypto.createHmac('sha1', SECRET).update(chunk).digest('hex')}`;
-            const isAllowed = req.headers['x-hub-signature'] === signature;
-            const body = JSON.parse(chunk);
-            const isApprovedBranch = body?.ref === `refs/heads/${BRANCH}`;
-
-            console.log(`isAllowed: ${isAllowed}`, `isApprovedBranch: ${isApprovedBranch} (exp: ${body?.ref} req: ${BRANCH})`);
-
-            if (isAllowed && isApprovedBranch) {
-                try {
-                    if (!isBusy) {
-                        console.log('Auto deploy started...');
-                        isBusy = true;
-                        await run(`cd ${SCRIPT_LOCATION} && bash deploy.sh`);
-                    }
-                } catch (error) {
-                    console.error(error);
-                }
-            }
+            body = JSON.parse(chunk);
         } catch (e) {
             console.log(JSON.stringify(e));
         }
+        const isApprovedBranch = body?.ref === `refs/heads/${BRANCH}`;
+        console.log(`isAllowed: ${isAllowed}`, `isApprovedBranch: ${isApprovedBranch} (exp: ${body?.ref} req: ${BRANCH})`);
+
+        if (isAllowed && isApprovedBranch) {
+            try {
+                if (!isBusy) {
+                    timestamp = Date.now();
+                    console.log('Auto deploy started...');
+                    isBusy = true;
+                    await run(`cd ${SCRIPT_LOCATION} && bash deploy.sh`);
+                    console.log('Finished! Time (ms): ', Date.now() - timestamp);
+                    isBusy = false;
+                }
+            } catch (error) {
+                isBusy = false;
+                console.error(error);
+            }
+        }
+
     });
 
-    console.log('Finished! Time (ms): ', Date.now() - timestamp);
-    isBusy = false;
     res.writeHead(200);
     res.end();
 }).listen(PORT);
