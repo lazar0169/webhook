95b1286e0f8f2d11d7a2d5682df5400447a5595c
lazar0169 <lazar0169@gmail.com>
2020-08-03 13:57:41 +0200

    Fixed eslint and removed prettier

--- index.js
+++ index.js
@@ -16,38 +16,46 @@ const SCRIPT_LOCATION = '~';
 let isBusy = false;
 let timestamp = 0;
 
-http.createServer((req, res) => {
-  req.on('data', async (chunk) => {
-    let body;
-    const signature = `sha1=${crypto.createHmac('sha1', SECRET).update(chunk).digest('hex')}`;
-    const isAllowed = req.headers['x-hub-signature'] === signature;
-    try {
-      body = JSON.parse(chunk);
-    } catch (e) {
-      log(JSON.stringify(e));
-    }
-    const isApprovedBranch = body?.ref === `refs/heads/${BRANCH}`;
-    log(`isAllowed: ${isAllowed}`, `isApprovedBranch: ${isApprovedBranch} (exp: ${body?.ref} req: ${BRANCH})`);
-
-    if (isAllowed && isApprovedBranch) {
+http
+  .createServer((req, res) => {
+    req.on('data', async (chunk) => {
+      let body;
+      const signature = `sha1=${crypto
+        .createHmac('sha1', SECRET)
+        .update(chunk)
+        .digest('hex')}`;
+      const isAllowed = req.headers['x-hub-signature'] === signature;
       try {
-        if (!isBusy) {
-          timestamp = Date.now();
-          log('Auto deploy started...');
-          isBusy = true;
-          await run(`cd ${SCRIPT_LOCATION} && bash deploy.sh`);
-          log('Finished! Time (ms): ', Date.now() - timestamp);
-          isBusy = false;
-        }
-      } catch (err) {
-        isBusy = false;
-        error(err);
+        body = JSON.parse(chunk);
+      } catch (e) {
+        log(JSON.stringify(e));
       }
-    }
-  });
+      const isApprovedBranch = body?.ref === `refs/heads/${BRANCH}`;
+      log(
+        `isAllowed: ${isAllowed}`,
+        `isApprovedBranch: ${isApprovedBranch} (exp: ${body?.ref} req: ${BRANCH})`,
+      );
 
-  res.writeHead(200);
-  res.end();
-}).listen(PORT);
+      if (isAllowed && isApprovedBranch) {
+        try {
+          if (!isBusy) {
+            timestamp = Date.now();
+            log('Auto deploy started...');
+            isBusy = true;
+            await run(`cd ${SCRIPT_LOCATION} && bash deploy.sh`);
+            log('Finished! Time (ms): ', Date.now() - timestamp);
+            isBusy = false;
+          }
+        } catch (err) {
+          isBusy = false;
+          error(err);
+        }
+      }
+    });
+
+    res.writeHead(200);
+    res.end();
+  })
+  .listen(PORT);
 
 log(`Listening for webhooks on: ${PORT}`);
--- time.js
+++ time.js
@@ -1,17 +1,32 @@
 class Time {
   /**
-     * @param format
-     * @param time
-     * @param UTC
-     * @return {string}
-     */
+   * @param format
+   * @param time
+   * @param UTC
+   * @return {string}
+   */
   static get(format = 'DD-MM-YYYY_h-m', time = Date.now(), UTC = true) {
     const dateObject = new Date(time);
-    const date = (UTC ? dateObject.getUTCDate() : dateObject.getDate()).toString();
-    const month = (UTC ? dateObject.getUTCMonth() + 1 : dateObject.getMonth()).toString();
-    const year = (UTC ? dateObject.getUTCFullYear() : dateObject.getFullYear()).toString();
-    const hours = (UTC ? dateObject.getUTCHours() : dateObject.getHours()).toString();
-    const min = (UTC ? dateObject.getUTCMinutes() : dateObject.getMinutes()).toString();
+    const date = (UTC
+      ? dateObject.getUTCDate()
+      : dateObject.getDate()
+    ).toString();
+    const month = (UTC
+      ? dateObject.getUTCMonth() + 1
+      : dateObject.getMonth()
+    ).toString();
+    const year = (UTC
+      ? dateObject.getUTCFullYear()
+      : dateObject.getFullYear()
+    ).toString();
+    const hours = (UTC
+      ? dateObject.getUTCHours()
+      : dateObject.getHours()
+    ).toString();
+    const min = (UTC
+      ? dateObject.getUTCMinutes()
+      : dateObject.getMinutes()
+    ).toString();
     return format
       .replace('YYYY', year)
       .replace('MM', month)
